71ba44aef50bf950a18e47f3623d1d297b8e0c59
Hans SCHWAL <hans.schwal@laposte.net>
2019-02-18 16:03:44 +0100

    travail fonctionnalité

--- controllers/composeController.php
+++ controllers/composeController.php
@@ -1,25 +1,31 @@
 <?php
 require '../models/database.php';
+require '../models/savedProgressions.php';
 require '../models/compositionCategory.php';
 
 if(isset($_POST['choose'])){
-//    $user = new users();
+    $savedProgression = new savedProgressions();
     
     $firstDegree = $_POST['firstDegree'];
     $secondDegree = $_POST['secondDegree'];
     $thirdDegree = $_POST['thirdDegree'];
     $fourthDegree = $_POST['fourthDegree'];
     $fifthDegree = $_POST['fifthDegree'];
+    $sixthDegree = $_POST['sixthDegree'];
+    $seventhDegree = $_POST['seventhDegree'];
     
-    $user->lastName = $lastName;
-    $user->firstName = $firstName;
-    $user->mail = $mail;
-    $user->userName = $userName;
-    $user->password = $password;
-    $user->id = $id;
     
-    if($user->deleteUsers()){
-        echo 'Suppression réussie';
+    $savedProgression->firstDegree = $firstDegree;
+    $savedProgression->secondDegree = $fsecondDegree;
+    $savedProgression->thirdDegree = $thirdDegree;
+    $savedProgression->fourthDegree = $fourthDegree;
+    $savedProgression->fithDegree = $fifthDegree;
+    $savedProgression->sixthDegree = $sixthDegree;
+    $savedProgression->seventhDegree = $seventhDegree;
+    $savedProgression->id = $id;
+    
+    if($savedProgression->saveProgression()){
+        echo 'Progression enregistrée';
     }
 }
 
--- models/savedProgressions.php
+++ models/savedProgressions.php
@@ -15,13 +15,27 @@
 class savedProgressions extends database {
     public $id;
     public $scaleName;
-    public $scaleDegrees;
+    public $firstChosenDegree;
+    public $secondChosenDegree;
+    public $thirdChosenDegree;
+    public $fourthChosenDegree;
     
     public function __construct(){
          parent::__construct();
     }
 //    __construct() est la seule fonction qui s'execute au moment de l'instanciation d'une classe   
-  
-    
+ 
+    public function saveProgression(){
+        $query = 'INSERT INTO `savedProgressions`(`scaleName`, `firstChosenDegree`, `secondChosenDegree`, `thirdChosenDegree`, `fourthChosenDegree`) VALUES (:scaleName, :firstChosenDegree, :secondChosenDegree, :thirdChosenDegree, :fourthChosenDegree)';
+        $saveProgression = $this->db->prepare($query);
+        $saveProgression->bindValue(':scaleName', $this->lastName, PDO::PARAM_STR);
+        $saveProgression->bindValue(':firstChosenDegree', $this->firstName, PDO::PARAM_STR);
+        $saveProgression->bindValue(':secondChosenDegree', $this->mail, PDO::PARAM_STR);
+        $saveProgression->bindValue(':thirdChosenDegree', $this->userName, PDO::PARAM_STR);
+        $saveProgression->bindValue(':fourthChosenDegree', $this->password, PDO::PARAM_STR);
+        if($saveProgression->execute()){
+            return true;
+        }
+    }    
     
 }
